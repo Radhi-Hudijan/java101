61238c47d452ef4cc9e78dcd22ca4201849e9798
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-10-31 15:31:03 +0100

    Add Main class with basic Hello World implementation and variable declarations

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // System.out.print("Hello World!2");
+        // System.out.print(4);
+        String name = "John";
+        String lastName = "Doe";
+        int age = 30;
+        float height = 5.8f;
+        boolean isAdult = true;
+        
+        String fullName= name + " " + lastName;
+
+        System.out.println("hi" +" "+ fullName);
+        // System.out.println(age);
+
+        int x ,y,z;
+        x=y=z=10;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
