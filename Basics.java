162bdec4d49ba9bc8f9fbb9989d2b8114a854d75
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-11-01 09:31:20 +0100

    Add Basics class with main method demonstrating variable usage and control structures

---
+++ Basics.java
@@ -0,0 +1,21 @@
+public class Basics {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int max;
+        max = Math.max(a, b);
+        System.out.println(max);
+
+        int count = 0;
+        while (count < 5) {
+            System.out.println(count);
+            count++;
+        }
+
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -9,21 +9,21 @@ public class Main {
         float height = 5.8f;
         boolean isAdult = true;
         System.out.println(name.length());
-        
-        String fullName= name + " " + lastName;
 
-        System.out.println("hi" +" "+ fullName);
+        String fullName = name + " " + lastName;
+
+        System.out.println("hi" + " " + fullName);
         // System.out.println(age);
 
-        int x ,y,z;
-        x=y=z=10;
+        int x, y, z;
+        x = y = z = 10;
         // System.out.println(x);
         // System.out.println(y);
         // System.out.println(z);
 
         int a = 10;
         int b = 20;
-         b += a ;
+        b += a;
         System.out.println(a);
         System.out.println(x);
         System.out.println(b);
