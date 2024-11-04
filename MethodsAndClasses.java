9723124e26c9bf5f7c35d35a3bbba1de1f9999d4
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-11-04 11:21:28 +0100

    Add MethodsAndClasses class with commented methods for greeting and age checking

---
+++ MethodsAndClasses.java
@@ -0,0 +1,24 @@
+public class MethodsAndClasses {
+    // static void sayHello() {
+    // System.out.println("Hello World!");
+    // }
+
+    // static void sayHello(String name) {
+    // System.out.println("Hello " + name);
+    // }
+
+    // static void checkAge(int age) {
+    // if (age < 18) {
+    // System.out.println("You are not an adult");
+    // } else {
+    // System.out.println("You are an adult");
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // sayHello();
+    // sayHello("John");
+    // checkAge(20);
+    // checkAge(10);
+    // }
+}
