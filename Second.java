694df225e423f5126c53632159715e7329b8e8d2
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-11-08 14:13:35 +0100

    Update Second class to include a final variable and modify main method for demonstration

--- Second.java
+++ Second.java
@@ -1,6 +1,13 @@
 class Second {
+    final int x = 10;
+
     public static void main(String[] args) {
-        MethodsAndClasses myobj = new MethodsAndClasses();
+        // MethodsAndClasses myobj = new MethodsAndClasses();
+        // System.out.println(myobj.x);
+
+        Second myobj = new Second();
+        myobj.x = 25;
+
         System.out.println(myobj.x);
     }
 }
